2f78fd11077ed390d247c74495113a79ce5e70ef
wenchiching <wenchiching@gmail.com>
2016-02-04 20:19:10 +0800

    add init_sqlite.php

---
+++ init_sqlite.php
@@ -0,0 +1,19 @@
+<?php
+$dbfile='sqlitedb.db';
+
+if ( file_exists($dbfile) ) {
+    echo "$dbfile exist!\n";
+    return 1;
+}
+$db = new SQLite3('sqlitedb.db');
+
+$db->exec('CREATE TABLE currency (
+            currency    TEXT,
+            spot_buy    FLOAT,
+            spot_sell   FLOAT,
+            cash_buy    FLOAT,
+            cash_sell   FLOAT,
+            date        TEXT
+            )');
+
+?>
