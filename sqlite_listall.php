1c47e928951abbd54897fbd6d82e38242b7ea875
wenchiching <wenchiching@gmail.com>
2016-02-04 23:17:29 +0800

    update listall

--- sqlite_listall.php
+++ sqlite_listall.php
@@ -3,7 +3,7 @@ require_once('sqlite_common.php');
 
 $db = new SQLite3($dbfile);
 
-$stmt = $db->prepare('SELECT * FROM currency');
+$stmt = $db->prepare('SELECT * FROM currency ORDER BY datetime DESC LIMIT 5');
 $results = $stmt->execute();
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
     print_r($row);
