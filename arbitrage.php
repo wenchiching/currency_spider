536d6659d3341f6e59c2e81b3d69e2dc9cd590e6
owen_wen <owen_wen@htc.com>
2016-02-16 21:23:10 +0800

    can find arbitrage between VISA, HSBC

--- arbitrage.php
+++ arbitrage.php
@@ -3,84 +3,59 @@ require_once('sqlite_common.php');
 
 $db = new SQLite3($dbfile);
 
-$stmt = $db->prepare('SELECT * FROM currency WHERE currency="USD" AND spot_buy!="" AND spot_sell!="" AND datetime BETWEEN datetime(\'now\', \'-6 day\') AND datetime(\'now\', \'localtime\') ORDER BY datetime DESC LIMIT 100;');
+$stmt = $db->prepare('SELECT * FROM currency WHERE currency="USD" AND spot_buy!="" AND spot_sell!="" AND datetime BETWEEN datetime(\'now\', \'-30 day\') AND datetime(\'now\', \'localtime\') ORDER BY datetime DESC;');
 $results = $stmt->execute();
 $datetime = "";
 $a_bank = array();
 $a_spot_sell = array();
 $a_spot_buy = array();
 $a_datetime = array();
+$result = array();
 $last = 0;
 
-function find_arbitrage($bank, $spot_sell, $spot_buy, $datetime) {
+function find_arbitrage($bank, $spot_buy, $spot_sell, $datetime) {
     if ( count($bank) == 0 ){
         return 0;
-    } else {
-        print("ERROR");
-        exit(1);
     }
-    $spot_sell_min = min($spot_sell);
-    $spot_buy_max = max($spot_buy);
-    if ( $spot_sell_min > $spot_buy_max ) {
+    $i = 0;
+    $visa_buy = 0;
+    $hsbc_sell = 0;
+    while ( $i < count($bank) ) {
+        if ( strcmp($bank[$i], "VISA") == 0 ){
+            $visa_buy = $spot_buy[$i];
+        }
+        if ( strcmp($bank[$i], "HSBC") == 0 ){
+            $hsbc_sell = $spot_sell[$i];
+        }
+        $i++;
+    }
+    if ( $visa_buy == 0 || $hsbc_sell == 0 ) {return 0;}
+    if ( $visa_buy > $hsbc_sell ){
+        print("visa_buy:".$visa_buy."\thsbc_sell:".$hsbc_sell."\n");
         $i = 0;
         while ( $i < count($bank) ) {
-            if ( ($spot_sell[$i] == $spot_sell_min) || ($spot_buy[$i] == $spot_buy_max) ) {
-                print($bank[$i]);
-                print("\t");
-                print($spot_sell[$i]);
-                print("\t");
-                print($spot_buy[$i]);
-                print("\t");
-                print($datetime[$i]);
-                print("\t");
-                print("!!!");
-                print("\n");
-            }
+            print($bank[$i]."\t".$spot_buy[$i]."\t".$spot_sell[$i]."\t".$datetime[$i]."\n");
             $i++;
         }
+        print("\n");
+        return ($visa_buy - $hsbc_sell);
     }
 }
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-    /*
-    print($row["bank"]);
-    print("\t");
-    print($row["currency"]);
-    print("\t");
-    print($row["spot_buy"]);
-    print("\t");
-    print($row["spot_sell"]);
-    print("\t");
-    print($row["datetime"]);
-    print("\n");
-    */
     $datetime = strtotime($row["datetime"]);
     if ( ($last != (int)($datetime/60) ) && $last != 0 ){
-        print($row["bank"]);
-        print("\t");
-        print($row["currency"]);
-        print("\t");
-        print($row["spot_buy"]);
-        print("\t");
-        print($row["spot_sell"]);
-        print("\t");
-        print($row["datetime"]);
-        print("\t");
-        print($datetime);
-        print("\t");
-        print($last);
-        print("\n");
         $last = (int)($datetime/60);
-        find_arbitrage($a_bank, $a_spot_sell, $a_spot_buy, $a_datetime);
+        $result[] = find_arbitrage($a_bank, $a_spot_buy, $a_spot_sell, $a_datetime);
         $a_bank = array();
         $a_spot_sell = array();
         $a_spot_buy = array();
         $a_datetime = array();
-    } else {
-        $a_bank[] = $row["bank"];
-        $a_spot_sell[] = $row["spot_sell"];
-        $a_spot_buy[] = $row["spot_buy"];
-        $a_datetime[] = $row["datetime"];
-        $last = (int)($datetime/60);
     }
+    $a_bank[] = $row["bank"];
+    $a_spot_sell[] = $row["spot_sell"];
+    $a_spot_buy[] = $row["spot_buy"];
+    $a_datetime[] = $row["datetime"];
+    $last = (int)($datetime/60);
 }
+printf(max($result));
 ?>
