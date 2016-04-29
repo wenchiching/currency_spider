51e9286d8809c74fb82be6a75b471ae01da8d7a3
wenchiching <wenchiching@gmail.com>
2016-04-29 16:42:34 +0800

    add main_visa.sh

--- main_visa.sh
+++ main_visa.sh
@@ -4,12 +4,11 @@ base_path="$(dirname `readlink -e $0`)"
 mkdir -p $base_path/logs/
 today="$(date +'%Y%m%d')"
 yesterday=$(date -d 'yesterday' +%Y%m%d)
-log=$base_path/logs/$today.log
-log_yesterday=$base_path/logs/$yesterday.log
+log=$base_path/logs/${today}_visa.log
+log_yesterday=$base_path/logs/${yesterday}_visa.log
 
 cd $base_path
-php spider_hsbc.php >>$log 2>&1
-php spider_bot.php >>$log 2>&1
+php spider_visa.php >>$log 2>&1
 
 if [ -e $log_yesterday ]; then
     gzip $log_yesterday
